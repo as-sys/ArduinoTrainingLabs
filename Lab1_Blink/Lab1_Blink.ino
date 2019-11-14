cd79127d89aa6a621cd587e78e0b9486f5f31673
Andrew Gowan <andrew_847@icloud.com>
2019-11-14 11:15:06 -0800

    Add initial program

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -5,11 +5,21 @@
  * This program reads a digital input on pin2 prints the results to the serial monitor
  */
 
+//digital pin2 has a push button attached to it
+// Using the Function pinMode to call the variable led and provide Output 
+int pushButton = 2;
 void setup() {
-  // Using the Function pinMode to call the variable led and provide Output 
-
+  //initializes serial communication at 9600 bits per second 
+  Serial.begin(9600);
+  //make the pushBotton pin an input
+  pinMode(pushButton, INPUT);
+     
 }
 
 void loop() {
-
+  // read the input pin 
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button
+  Serial.println(buttonState);
+  delay(250); // delay in between readings for stability 
 }
