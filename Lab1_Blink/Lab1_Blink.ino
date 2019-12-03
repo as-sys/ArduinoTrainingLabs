86bd8c084773695e054b2b7f513bb6c120c9bbd6
Andrew Gowan <andrew_847@icloud.com>
2019-12-03 11:07:28 -0800

    Initial Potentiometer Program

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,43 +1,15 @@
 /* Programmer: Andrew Gowan
- *  Date: 11.6.19 
- *  Program: Blink
+ *  Date: 12.3.19
+ *  Program: Potentiameter 
  *  
- * This program will turn on an LED for specified amounts of time, turn it 
- * off, and back on in a While Loop.
- * 
- * Merged on 12.2.19
- * 
- * Programmer: Andrew Gowan
- * Date: 11.14.19
- * Program: digitalRead() & the Serial Port
- * 
- * This program reads a digital input on pin2 prints the results to the serial monitor 
+ *  AnalogReadSerial
+ *  Reads an analog input on pin A0, prints to the serial monitor 
+ *  
  */
 
-//initialize our led to Pin 13
-int led = 13;
-int pushButton = 2;
 void setup() {
-  // Using the Function pinMode to call the variable led and provide Output 
-  pinMode(led, OUTPUT);
 
-  //initializes serial communication at 9600 bits per second 
-  Serial.begin(9600);
-  //make the pushBotton pin an input
-  pinMode(pushButton, INPUT);
-}
-
+ {
 void loop() {
-    // read the input pin 
-  int buttonState = digitalRead(pushButton);
-  // print out the state of the button
-  Serial.println(buttonState);
-  // if buttonState is True/Pushed by human the LED will blink
-  if (buttonState==HIGH){
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led, LOW);
-  }
-  delay(1); // delay in between readings for stability 
+
 }
